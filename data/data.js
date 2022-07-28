9ec35b8b1e50396f71cf97e3ba0da6826f2e3bfb
Maria2214220 <102614834+Maria2214220@users.noreply.github.com>
2022-07-28 09:51:26 -0500

    Desarrollo: Programacion Proyecto Poo en java. Maria2214220

---
+++ data/data.js
@@ -0,0 +1,59 @@
+export const data = [
+    {
+      question: "¿Qué contiene una Clase?",
+      choices: ["Individuos", "Campos de datos, variables, métodos", "Similitudes entre objetos"],
+      answer: "Campos de datos, variables, métodos",
+    },
+    {
+      question: "¿Qué ciclo se usas más?",
+      choices: ["IF-ELSE", "SWITCH", "FOR"],
+      answer: "FOR",
+    },
+    {
+      question: "¿Cuáles son las características de la POO?",
+      choices: ["Distribuido, interpretado robusto, multihilo", "Ninguna", "Objetos, abstracción, polimorfismo, encapsulación"],
+      answer: "Objetos, abstracción, polimorfismo, encapsulación",
+    },
+    {
+      question: "¿Cuál de los siguientes define una variable?",
+      choices: ["COUT", "INT", "VOID"],
+      answer: "INT",
+      },
+
+    {
+      question: "En la programación orientada a objetos (POO), un objeto tiene las siguientes características: -Identidad. -Estado. -Comportamiento.-Mensajes. -Métodos.",
+      choices: ["Falso", "Verdadero"],
+      answer: "Verdadero",
+    },
+
+    {
+      question: "¿Cómo se conforman los objetos?",
+      choices: ["Se forman con datos (atributos) protegidos y datos privados", "Se forman con clases y subclases","Se forman con datos (atributos) y métodos."],
+      answer: "Se forman con datos (atributos) y métodos.",
+    },
+
+    {
+        question: "¿Cómo está formado un programa bajo el paradigma de orientación a objetos?",
+        choices: [" Conjunto de métodos que interactúan de forma conjunta."," Conjunto de objetos que interactúan de forma conjunta.","Conjunto de líneas de código que interactúan de forma conjunta"],
+        answer: "Conjunto de objetos que interactúan de forma conjunta",
+      },
+
+      {
+        question: "Un programa es una serie de órdenes o instrucciones ordenadas con unafinalidad concreta que realizan una función determinada",
+        choices: ["Verdadero","Falso"],
+        answer: "Verdadero",
+      },
+
+      {
+      question: "El código fuente es un texto escrito en un lenguaje de programaciónespecífico y que puede ser entendido por un programador",
+      choices: ["Verdadero", "Falso"],
+      answer: "Verdadero",
+      },
+
+      {
+      question: "¿Qué contiene una clase (poo)?",
+      choices: [" Una clase incluye la superposición cuántica de las partículas que nos rodean", " Una clase incluye alumnos","Una clase incluye los datos y el código que operará sobre esos datos"],
+      answer: "Una clase incluye los datos y el código que operará sobre esos datos",
+      },
+
+  ];
